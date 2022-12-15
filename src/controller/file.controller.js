ba24326c937e73d93c18ae88445feba1caa91d3a
jamin12 <rudals9901@naver.com>
2022-12-15 16:32:16 +0900

    💄 Style: imgserver url 변경

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -3,11 +3,8 @@
 const logger = require("../config/logger");
 const catchAsync = require("../utils/catchAsync");
 const { fileService } = require("../service/index");
-const passport = require("passport");
 const resultDto = require("../dto/resultDTO");
 const httpStatus = require("http-status");
-const { readFile, readFileSync } = require("fs");
-const { Blob } = require("buffer");
 
 const fileservice = new fileService();
 
@@ -15,7 +12,7 @@ const output = {
 	getFile: catchAsync(async (req, res) => {
 		const fileName = await fileservice.getImgFile(req.params.fid);
 		res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-		res.redirect(`http://39.120.8.109:3551/images/${fileName.filename}`);
+		res.redirect(`https://www.jaminimg.shop/images/${fileName.filename}`);
 	}),
 };
 
